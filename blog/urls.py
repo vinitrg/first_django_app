598b2d8f9278f74ec1ecb109cd29b877ed585c50
vinitrg <vinit.gujarathi@gmail.com>
2018-08-11 23:08:33 +0530

    Working on views, experimenting with different function 'publish_post' and making changes in regard to same in urls.py and views.py, changes in post_list.html to accomodate list of posts.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.post_list, name='post_list')
+	path('',views.publish_list, name='publish_list')
+	#path('',views.post_list, name='post_list')
 ]
